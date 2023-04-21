c30406aa470f658e1239feb5d97b09258ee7353b
ShivNandan-Coder <shivnandansahu5@gmail.com>
2023-04-21 11:18:15 +0530

    initial commit

--- src/main/java/com/example/getpostdeleteApi/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-/*package com.example.getpostdeleteApi.repository;
-import org.springframework.data.repository.CrudRepository;
-import com.example.getpostdeleteApi.model.Student;
-public interface StudentRepository extends CrudRepository<Student, Integer>
-{
-}
-*/
